920322dc1d72732d3e512ffbce3af944578b3d92
KEVPXX <cc.privada.2000@gmail.com>
2026-05-10 22:04:52 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE='pochos-v36';
+const CACHE='pochos-v42';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{
   e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener('fetch',e=>{
-  if(e.request.url.includes('firebase')||e.request.url.includes('googleapis'))return;
+  if(e.request.url.includes('firebase')||e.request.url.includes('googleapis')||e.request.url.includes('fonts.g'))return;
   e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
